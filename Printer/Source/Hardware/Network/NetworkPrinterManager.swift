ff0e82abd771b8419ff083734241ae17480b24f8
Gueoff <geoffrey.desbrosses@gmail.com>
2024-09-19 16:59:54 +0200

    add network strategy

--- Printer/Source/Hardware/Network/NetworkPrinterManager.swift
+++ Printer/Source/Hardware/Network/NetworkPrinterManager.swift
@@ -1,14 +1,22 @@
 //
-//  PrinterManager.swift
+//  NetworkPrinterManager.swift
 //  Printer
 //
-//  Created by gix on 12/8/16.
-//  Copyright © 2016 Kevin. All rights reserved.
+//  Created by Geoffrey Desbrosses on 19/09/2024.
+//  Copyright © 2024 Kevin. All rights reserved.
 //
 
 import Foundation
+import Network
+import MobileCoreServices
 
 public class NetworkPrinterManager {
+    private var networkConnection: NWConnection?
+    
+    public enum TicketPrintError: Error {
+        case networkError(NWError)
+        case unknownError
+    }
 
     public func connectNetwork(ip: String, port: Int) {
         let PORT = NWEndpoint.Port(String(port))
@@ -32,15 +40,44 @@ public class NetworkPrinterManager {
     networkConnection?.start(queue: queue)
   }
 
-    public func print(_ ticket: Ticket) {
+    public func print(_ ticket: Ticket) throws {
         let content = getTicketData(ticket)
+        
+        guard let connection = networkConnection else {
+            throw TicketPrintError.unknownError
+        }
 
-        networkConnection?.send(content: content, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
-            if (NWError == nil) {
-                print("Data was sent to TCP destination ")
-            } else {
-                print("ERROR! Error when data (Type: Data) sending. NWError: \n \(NWError!)")
+        let dispatchGroup = DispatchGroup()
+        var printError: Error?
+
+        dispatchGroup.enter()
+
+        connection.send(content: content, completion: NWConnection.SendCompletion.contentProcessed({ (nwError) in
+            if let error = nwError {
+                printError = TicketPrintError.networkError(error)
             }
-        })))
-   }
+            dispatchGroup.leave()
+        }))
+
+        dispatchGroup.wait()
+
+        if let error = printError {
+            throw error
+        }
+    }
+    
+    private func getTicketData(_ ticket: Ticket) -> Data {
+      let encoding = String.Encoding.utf8
+      let ticketData = ticket.data(using: encoding)
+      var combinedData = ticketData.reduce(Data()) { (result, data) -> Data in
+        var mutableResult = result
+        mutableResult.append(data)
+        return mutableResult
+      }
+
+        let paperCutCommand: [UInt8] = [0x1D, 0x56, 0x00]
+        combinedData.append(Data(paperCutCommand))
+
+      return combinedData
+    }
 }
